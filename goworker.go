0a24dbc860fc6a0b24d423021bd0af207b91b209
Andrew Stucki <andrew.stucki@gmail.com>
2013-11-12 02:55:06 -0600

    update OOP

--- goworker.go
+++ goworker.go
@@ -8,23 +8,56 @@ import (
 	"time"
 )
 
-var logger seelog.LoggerInterface
+var (
+  logger seelog.LoggerInterface
+	queuesString   string
+	queues         queuesFlag
+	intervalFloat  float64
+	interval       intervalFlag
+	concurrency    int
+	connections    int
+	uri            string
+	namespace      string
+	exitOnComplete bool
+	isStrict       bool
+)
+
+type Worker struct {
+  Queues  string,
+  IntervalFloat float64,
+  Concurrency int
+  Connections int
+  Uri string
+  Namespace string
+  ExitOnComplete  bool
+}
 
 // Call this function to run goworker. Check for errors in
 // the return value. Work will take over the Go executable
 // and will run until a QUIT, INT, or TERM signal is
 // received, or until the queues are empty if the
 // -exit-on-complete flag is set.
-func Work() error {
+func (w *Worker) Work() error {
 	var err error
 	logger, err = seelog.LoggerFromWriterWithMinLevel(os.Stdout, seelog.InfoLvl)
 	if err != nil {
 		return err
 	}
 
-	if err := flags(); err != nil {
+  queuesString = w.Queues
+  intervalFloat = w.IntervalFloat
+  concurrency = w.Concurrency
+  uri = w.Uri
+  namespace = w.Namespace
+  exitOnComplete = w.ExitOnComplete
+
+	if err := queues.Set(queuesString); err != nil {
 		return err
 	}
+	if err := interval.SetFloat(intervalFloat); err != nil {
+		return err
+	}
+	isStrict = strings.IndexRune(queuesString, '=') == -1
 
 	quit := signals()
 
